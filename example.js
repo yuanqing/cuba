9680bc408ed3965e09a6f297b1b391cf7ca83101
Yuan Qing <desk@yuanqing.sg>
2018-04-07 11:13:18 +0800

    Fix name

--- example.js
+++ example.js
@@ -2,6 +2,6 @@ const cuba = require('./')
 
 const database = cuba('1tXLr47ArNQjICWWDtXCABXPH__rlK0yxPsfAzpqysi8')
 ;(async function () {
-  const result = await database.query('select *', { sheetName: 'Sheet2' })
+  const result = await database.query('select *', { sheetName: 'Sheet 2' })
   console.log(result)
 })()
