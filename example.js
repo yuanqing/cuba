5bbb1d02a0c4d6e7040f3b08fa26bfa709c27c64
Yuan Qing <desk@yuanqing.sg>
2018-04-07 11:47:47 +0800

    Edit example

--- example.js
+++ example.js
@@ -1,7 +1,6 @@
 const cuba = require('./')
-
-const database = cuba('1tXLr47ArNQjICWWDtXCABXPH__rlK0yxPsfAzpqysi8')
 ;(async function () {
+  const database = cuba('1tXLr47ArNQjICWWDtXCABXPH__rlK0yxPsfAzpqysi8')
   const query = 'select *'
   const options = { sheetName: 'Sheet 2' }
   const result = await database.query(query, options)
