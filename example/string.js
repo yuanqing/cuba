c841f9282a223b333bc76291ace48fa962ca6eaf
Yuan Qing <desk@yuanqing.sg>
2018-04-15 12:45:31 +0800

    Rename variable

--- example/string.js
+++ example/string.js
@@ -1,6 +1,6 @@
 const cuba = require('..')
 ;(async function () {
   const spreadsheet = await cuba('1InLekepCq4XgInfMueA2E2bqDqICVHHTXd_QZab0AOU')
-  const results = await spreadsheet.query('select *')
-  console.log(results)
+  const array = await spreadsheet.query('select *')
+  console.log(array)
 })()
