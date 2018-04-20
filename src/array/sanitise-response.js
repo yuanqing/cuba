6d0bd33120e993e05819af97426ae63bc2f6844d
Yuan Qing <desk@yuanqing.sg>
2018-04-20 19:43:01 +0800

    Stop passing in `X-DataSource-Auth`

--- src/array/sanitise-response.js
+++ src/array/sanitise-response.js
@@ -1,5 +1,6 @@
-const responsePrefixJunkRegex = /^\)]\}'\n/
+const startLength = '/*O_o*/\ngoogle.visualization.Query.setResponse('.length
+const endLength = ');'.length
 
 module.exports = function (response) {
-  return response.replace(responsePrefixJunkRegex, '')
+  return response.substring(startLength, response.length - endLength)
 }
--- src/fetch.js
+++ src/fetch.js
@@ -1,14 +1,9 @@
 const fetch = require('isomorphic-unfetch')
 
 module.exports = async function (url, accessToken) {
-  const headers = {
-    'X-DataSource-Auth': 'true'
-  }
-  if (accessToken) {
-    // https://developers.google.com/identity/protocols/OAuth2ServiceAccount#callinganapi
-    headers['Authorization'] = `Bearer ${accessToken}`
-  }
-  return fetch(url, {
-    headers
+  return fetch(url, accessToken && {
+    headers: {
+      Authorization: `Bearer ${accessToken}`
+    }
   })
 }
