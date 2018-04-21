ca31333724d83216a2d33a697436fc369a78db22
Yuan Qing <desk@yuanqing.sg>
2018-04-21 10:26:38 +0800

    Lint

--- src/fetch.js
+++ src/fetch.js
@@ -1,9 +1,12 @@
 const fetch = require('isomorphic-unfetch')
 
 module.exports = async function (url, accessToken) {
-  return fetch(url, accessToken && {
-    headers: {
-      Authorization: `Bearer ${accessToken}`
+  return fetch(
+    url,
+    accessToken && {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
     }
-  })
+  )
 }
