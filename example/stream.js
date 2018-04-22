971034cc6ad82dc88139168be1a5bea63b27cb1b
Yuan Qing <desk@yuanqing.sg>
2018-04-22 21:04:19 +0800

    Edit `require` calls

--- example/stream.js
+++ example/stream.js
@@ -1,8 +1,8 @@
-const cuba = require('..')
+const cuba = require('..').stream
 const Transform = require('stream').Transform
 
 async function main () {
-  const query = await cuba.stream(
+  const query = await cuba(
     '1InLekepCq4XgInfMueA2E2bqDqICVHHTXd_QZab0AOU'
   )
   const stream = await query('select *')
--- test/stream.js
+++ test/stream.js
@@ -1,7 +1,7 @@
 const concat = require('concat-stream')
 const test = require('tape')
 
-const cuba = require('..')
+const cuba = require('..').stream
 const getServiceAccountCredentials = require('./utils/get-service-account-credentials')
 
 const publicSpreadsheetId = '1InLekepCq4XgInfMueA2E2bqDqICVHHTXd_QZab0AOU'
@@ -11,7 +11,7 @@ const serviceAccountCredentials = getServiceAccountCredentials()
 test('throws if no `id` specified', async function (t) {
   t.plan(1)
   try {
-    await cuba.stream()
+    await cuba()
   } catch (error) {
     t.pass()
   }
@@ -19,7 +19,7 @@ test('throws if no `id` specified', async function (t) {
 
 test('returns the entire contents of the first sheet', async function (t) {
   t.plan(1)
-  const query = await cuba.stream(publicSpreadsheetId)
+  const query = await cuba(publicSpreadsheetId)
   const stream = await query()
   const expected = [
     { id: 1, name: 'foo' },
@@ -35,7 +35,7 @@ test('returns the entire contents of the first sheet', async function (t) {
 
 test('runs a query, defaulting to the first sheet', async function (t) {
   t.plan(1)
-  const query = await cuba.stream(publicSpreadsheetId)
+  const query = await cuba(publicSpreadsheetId)
   const stream = await query('select * where A > 1')
   const expected = [{ id: 2, name: 'bar' }, { id: 3, name: 'baz' }]
   stream.pipe(
@@ -47,7 +47,7 @@ test('runs a query, defaulting to the first sheet', async function (t) {
 
 test('throws if the query is invalid', async function (t) {
   t.plan(1)
-  const query = await cuba.stream(publicSpreadsheetId)
+  const query = await cuba(publicSpreadsheetId)
   const stream = await query('qux')
   stream.on('error', function () {
     t.pass()
@@ -56,7 +56,7 @@ test('throws if the query is invalid', async function (t) {
 
 test('runs the query on the sheet with the specified sheet name', async function (t) {
   t.plan(1)
-  const query = await cuba.stream(publicSpreadsheetId)
+  const query = await cuba(publicSpreadsheetId)
   const stream = await query('select *', { sheetName: 'Sheet2' })
   const expected = [{ A: 1, B: 42 }, { A: 2, B: 3142 }]
   stream.pipe(
@@ -68,7 +68,7 @@ test('runs the query on the sheet with the specified sheet name', async function
 
 test('runs the query on the sheet with the specified sheet ID', async function (t) {
   t.plan(1)
-  const query = await cuba.stream(publicSpreadsheetId)
+  const query = await cuba(publicSpreadsheetId)
   const stream = await query('select *', { sheetId: '224335590' })
   const expected = [{ id: 1, sum: 31 }, { id: 2, sum: 4215 }, { id: 3, sum: 1 }]
   stream.pipe(
@@ -81,7 +81,7 @@ test('runs the query on the sheet with the specified sheet ID', async function (
 if (serviceAccountCredentials) {
   test('runs a query on a spreadsheet via a Service Account', async function (t) {
     t.plan(1)
-    const query = await cuba.stream(
+    const query = await cuba(
       privateSpreadsheetId,
       serviceAccountCredentials
     )
@@ -100,7 +100,7 @@ if (serviceAccountCredentials) {
 
   test('allows the Service Account credentials to be specified using `camel_case` keys', async function (t) {
     t.plan(1)
-    const query = await cuba.stream(privateSpreadsheetId, {
+    const query = await cuba(privateSpreadsheetId, {
       client_email: serviceAccountCredentials.clientEmail,
       private_key: serviceAccountCredentials.privateKey
     })
