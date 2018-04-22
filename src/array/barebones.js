f91bea74e5476cd9f85b089dbf92dbc59f5e43a4
Yuan Qing <desk@yuanqing.sg>
2018-04-22 22:05:52 +0800

    Edit `array-barebones` API

--- src/array/barebones.js
+++ src/array/barebones.js
@@ -5,18 +5,16 @@ const identity = require('../identity')
 const parse = require('./parse')
 const sanitiseResponse = require('./sanitise-response')
 
-module.exports = async function (spreadsheetId) {
+module.exports = async function (spreadsheetId, query, options) {
   if (spreadsheetId == null) {
     throw new Error('Need a spreadsheet ID')
   }
-  return async function (query, options) {
-    const url = buildUrl(spreadsheetId, query || defaultQuery, options)
-    const response = await fetch(url)
-    const text = sanitiseResponse(await response.text())
-    const json = JSON.parse(text)
-    if (json.errors) {
-      throw new Error(json.errors[0].detailed_message)
-    }
-    return parse(json.table, (options && options.transform) || identity)
+  const url = buildUrl(spreadsheetId, query || defaultQuery, options)
+  const response = await fetch(url)
+  const text = sanitiseResponse(await response.text())
+  const json = JSON.parse(text)
+  if (json.errors) {
+    throw new Error(json.errors[0].detailed_message)
   }
+  return parse(json.table, (options && options.transform) || identity)
 }
--- test/array-barebones.js
+++ test/array-barebones.js
@@ -15,8 +15,7 @@ test('throws if no `id` specified', async function (t) {
 
 test('returns the entire contents of the first sheet', async function (t) {
   t.plan(1)
-  const query = await cuba(publicSpreadsheetId)
-  const actual = await query()
+  const actual = await cuba(publicSpreadsheetId)
   const expected = [
     { id: 1, name: 'foo' },
     { id: 2, name: 'bar' },
@@ -27,17 +26,15 @@ test('returns the entire contents of the first sheet', async function (t) {
 
 test('runs a query, defaulting to the first sheet', async function (t) {
   t.plan(1)
-  const query = await cuba(publicSpreadsheetId)
-  const actual = await query('select * where A > 1')
+  const actual = await cuba(publicSpreadsheetId, 'select * where A > 1')
   const expected = [{ id: 2, name: 'bar' }, { id: 3, name: 'baz' }]
   t.deepEqual(actual, expected)
 })
 
 test('throws if the query is invalid', async function (t) {
   t.plan(1)
-  const query = await cuba(publicSpreadsheetId)
   try {
-    await query('qux')
+    await cuba(publicSpreadsheetId, 'qux')
   } catch (error) {
     t.true(/^Error: Invalid query/.test(error.toString()))
   }
@@ -45,16 +42,14 @@ test('throws if the query is invalid', async function (t) {
 
 test('runs the query on the sheet with the specified sheet name', async function (t) {
   t.plan(1)
-  const query = await cuba(publicSpreadsheetId)
-  const actual = await query('select *', { sheetName: 'Sheet2' })
+  const actual = await cuba(publicSpreadsheetId, 'select *', { sheetName: 'Sheet2' })
   const expected = [{ A: 1, B: 42 }, { A: 2, B: 3142 }]
   t.deepEqual(actual, expected)
 })
 
 test('runs the query on the sheet with the specified sheet ID', async function (t) {
   t.plan(1)
-  const query = await cuba(publicSpreadsheetId)
-  const actual = await query('select *', { sheetId: '224335590' })
+  const actual = await cuba(publicSpreadsheetId, 'select *', { sheetId: '224335590' })
   const expected = [{ id: 1, sum: 31 }, { id: 2, sum: 4215 }, { id: 3, sum: 1 }]
   t.deepEqual(actual, expected)
 })
