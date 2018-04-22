7a75f11f06728939a5c108e5f822837c074e14d7
Yuan Qing <desk@yuanqing.sg>
2018-04-22 22:35:49 +0800

    Lint

--- example/micro.js
+++ example/micro.js
@@ -1,7 +1,10 @@
 const cuba = require('../src/micro')
 
 async function main () {
-  const array = await cuba('1InLekepCq4XgInfMueA2E2bqDqICVHHTXd_QZab0AOU', 'select *')
+  const array = await cuba(
+    '1InLekepCq4XgInfMueA2E2bqDqICVHHTXd_QZab0AOU',
+    'select *'
+  )
   console.log(array)
   // => [
   // =>   { id: 1, name: 'foo' },
